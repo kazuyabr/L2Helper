fa545434badcd41dff24c1d0a99c10bae7413dcb
FazZzuR <fazzzur@mail.ru>
2016-02-18 01:36:34 +0300

    improving assembly for testing and release

--- build.bat
+++ build.bat
@@ -1,5 +1,3 @@
 @echo off
-"C:\Program Files (x86)\AutoIt3\Aut2Exe\Aut2exe.exe" /in L2helper.au3 /out compiled\L2Helper.exe /icon res\l2.ico
-"C:\Program Files (x86)\AutoIt3\Aut2Exe\Aut2exe.exe" /in Launcher.au3 /out compiled\Launcher.exe /icon res\l2.ico
-copy config.ini compiled\config.ini /y
-"C:\Program Files\WinRAR\rar.exe" a compiled\L2Helper.rar compiled\*.* -ep -df
+"C:\Program Files (x86)\AutoIt3\Aut2Exe\Aut2exe.exe" /in L2helper.au3 /out program\L2Helper.exe /icon res\l2.ico
+"C:\Program Files (x86)\AutoIt3\Aut2Exe\Aut2exe.exe" /in Launcher.au3 /out program\Launcher.exe /icon res\l2.ico
